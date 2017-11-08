24957ff846a84d81b214efb6e4df8b7d90da195c
Aikman Ong <aikmanong@gmail.com>
2017-11-07 18:04:37 -0800

    test
    testing

--- Tetris/TetrisGrid.cs
+++ Tetris/TetrisGrid.cs
@@ -10,6 +10,21 @@ namespace Tetris
 {
     class TetrisGrid
     {
-        int[,] gameGrid = new int [10,20]; //20 x 10 Tetris Grid 
+        int[,] gameGrid = new int [9,17]; //9x17 Tetris Grid 
+        int[,] block = new int[4, 4]; //Pieces
+
+        /// <summary>
+        /// Purpose of this method is to reset the game board to all zeros.
+        /// </summary>
+        public void NewGame()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j =0; j <17; j++)
+                {
+                    gameGrid[i, j] = 0;
+                }
+            }
+        }
     }
 }
