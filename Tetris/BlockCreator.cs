c5f479097707a750695bd2d74ea61725316378ff
ginoesco <g.escolano91@gmail.com>
2017-11-08 22:43:33 -0800

    test

--- Tetris/BlockCreator.cs
+++ Tetris/BlockCreator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft. 
 
 namespace Tetris
-{
-    class BlockCreator
+{   class BlockCreator
     {
+        
         int[,] block = new int[4, 4]; //Pieces
 
+        
     }
 }
